080219786b5cc7006cdb37947ad1dfb098a34c7a
kinga97 <kinga.kucsera97@gmail.com>
2020-09-10 09:27:06 +0200

    Harmadik commit

---
+++ alap.js
@@ -0,0 +1,20 @@
+'use strict'; // Gyengén típusos nyelv (Adattípusai: number, bigint, string, boolean, null, undefined, object, symbol)
+
+let nev = 'Kucsera Kinga';
+/* Modális ablak függvények */
+// window.alert(nev);
+// alert(nev);
+
+// let tanuloSzam = window.prompt('Hány tanuló van jelen:', 12);
+// let szoveg = `A tanulók száma: ${tanuloSzam}`;
+
+// let szoveg = 1232344567565756775755765765767576577576576756757576775757n * 2n; //Bigint
+// let szoveg; //Undefined
+
+// let szoveg = 12;
+// szoveg = null; //Null
+
+let szoveg = 'Hello World!';
+// window.alert(szoveg);
+
+document.getElementById('kiir').innerText = szoveg;
